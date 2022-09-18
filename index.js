544b67998ede3b4718c544a3f556070c695141ca
Megan Rowan <megan.rowan.88@gmail.com>
2022-09-18 14:43:38 -0400

    commit tree 4dffd250552f7ec7630104690907fcb855af979d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(names, event) {
+    let message=[];
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift`)
+    }
+    return message;
+}
+
+writeCards(names, event);
